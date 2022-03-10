b7c78057ce0ba7804757a2d14304beaa939d6d93
Alexey Litovchenko <alex.lit@outlook.com>
2022-03-10 14:57:37 +0300

    chore: preparing for version update

--- plugins/jsdoc.js
+++ plugins/jsdoc.js
@@ -2,8 +2,6 @@
  * @see [prettier-plugin-jsdoc](https://github.com/hosseinmd/prettier-plugin-jsdoc)
  */
 module.exports = {
-  jsdocPreferCodeFences: true,
-  jsdocSeparateReturnsFromParam: true,
   jsdocSeparateTagGroups: true,
   jsdocSingleLineComment: false,
 };
