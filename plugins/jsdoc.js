667c6007a8f9839dcbdcd8cb442ebeebcc11239f
Alexey Litovchenko <alex.lit@outlook.com>
2022-08-24 22:31:10 +0300

    chore: preparing for version update

--- plugins/jsdoc.js
+++ plugins/jsdoc.js
@@ -2,6 +2,7 @@
  * @see [prettier-plugin-jsdoc](https://github.com/hosseinmd/prettier-plugin-jsdoc)
  */
 module.exports = {
+  jsdocSeparateReturnsFromParam: true,
   jsdocSeparateTagGroups: true,
   jsdocSingleLineComment: false,
 };
--- test/jsdoc.js
+++ test/jsdoc.js
@@ -1,19 +1,20 @@
-/* eslint-disable jsdoc/tag-lines, unicorn/no-empty-file */
+/* eslint-disable unicorn/no-empty-file */
 /**
  * In these configuration files, the rule plugin1/rule1 comes from the plugin
- * named plugin1. You can also use this format with configuration comments, such as:
+ * named plugin1. You can also use this format with configuration comments, such
+ * as:
  *
  * Hello.
  *
  * @example
- *   ```html
- *           <h1>test</h1>
- *   ```;
+ *   <h1>test</h1>;
+ *
  *
  * @type {React.FC<{ message: string }>}
  *
  * @param x Test
- * @param {any} var An example text that is long and spans lines followed by a list
+ * @param {any} var An example text that is long and spans lines followed by a
+ *   list
  *
  *   - Item 1.
  *   - Item 2.
@@ -23,3 +24,10 @@
  *
  * @returns {Promise} A return value.
  */
+
+/**
+ * Test
+ *
+ * Start
+ */
+export const x = 1;
